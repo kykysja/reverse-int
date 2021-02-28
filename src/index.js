00cc2161315ac057b0090bed1ac2af5f01b61314
kykysja <kykysja11@gmail.com>
2021-02-28 19:57:32 +0100

    feat: implement the function of the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+	if (n > 0) {
+		n = String(n);
+		n = n.split('').reverse().join('');
+	} else {
+		n = String(n);
+		n = n.split('').reverse().slice(0, -1).join('');
+	}
+	return +n;
 }
